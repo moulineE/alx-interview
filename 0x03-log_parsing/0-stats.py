9da63996f46817dba9b82a634c7828b9ba5fc240
moulineE <elmahdi.mouline@live.fr>
2024-01-25 23:19:28 +0100

    Task 0. Log parsing -teste 1

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,10 +5,13 @@ import re
 
 
 if __name__ == '__main__':
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    codes = {
+        "200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0
+    }
     total_size = 0
     pattern = (r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} -'
-               r' \[.*] "GET /projects/260 HTTP/1.1" \d{3,3} \d{1,}')
+               r' \[.*] "GET /projects/260 HTTP/1.1" (.{3}) \d{1,}')
     count = 0
 
     def print_logs_stats(codes: dict, total_size: int) -> None:
@@ -20,7 +23,7 @@ if __name__ == '__main__':
         print("File size: {:d}".format(total_size))
         for code in codes:
             if codes[code] != 0:
-                print("{:d}: {:d}".format(code, codes[code]))
+                print("{}: {:d}".format(code, codes[code]))
 
     try:
         for line in sys.stdin:
@@ -29,7 +32,7 @@ if __name__ == '__main__':
                 data = line.split()
                 try:
                     total_size += int(data[-1])
-                    code = int(data[-2])
+                    code = data[-2]
                     if code in codes:
                         codes[code] += 1
                 except BaseException:
