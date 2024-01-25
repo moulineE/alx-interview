61015edcfb0a1d1efcdd61f0185278de5fe3e3ed
moulineE <elmahdi.mouline@live.fr>
2024-01-25 22:40:09 +0100

    Task 0. Log parsing -req fix4

--- 0x03-log_parsing /0-stats.py
+++ 0x03-log_parsing /0-stats.py
@@ -11,29 +11,29 @@ pattern = (r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} -'
 count = 0
 
 
-def print_logs_stats(codes: dict, total_size: int) -> None:
-    """print the logs stats
-    Args:
-        codes (dict): the status codes and frequency
-        total_size (int): the total size of the logs
-    """
-    print("File size: {:d}".format(total_size))
-    for code in codes:
-        if codes[code] != 0:
-            print("{:d}: {:d}".format(code, codes[code]))
+if __name__ == '__main__':
+    def print_logs_stats(codes: dict, total_size: int) -> None:
+        """print the logs stats
+        Args:
+            codes (dict): the status codes and frequency
+            total_size (int): the total size of the logs
+        """
+        print("File size: {:d}".format(total_size))
+        for code in codes:
+            if codes[code] != 0:
+                print("{:d}: {:d}".format(code, codes[code]))
 
-
-try:
-    for line in sys.stdin:
-        if re.match(pattern, line):
-            count += 1
-            data = line.split()
-            total_size += int(data[-1])
-            code = int(data[-2])
-            if code in codes:
-                codes[code] += 1
-        if count % 10 == 0:
-            print_logs_stats(codes, total_size)
-    print_logs_stats(codes, total_size)
-finally:
-    print_logs_stats(codes, total_size)
+    try:
+        for line in sys.stdin:
+            if re.match(pattern, line):
+                count += 1
+                data = line.split()
+                total_size += int(data[-1])
+                code = int(data[-2])
+                if code in codes:
+                    codes[code] += 1
+            if count % 10 == 0:
+                print_logs_stats(codes, total_size)
+        print_logs_stats(codes, total_size)
+    finally:
+        print_logs_stats(codes, total_size)
