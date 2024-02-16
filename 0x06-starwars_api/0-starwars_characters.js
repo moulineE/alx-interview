93edd1428e6175bbb8a5fdb385ce749a18a3dbb5
moulineE <elmahdi.mouline@live.fr>
2024-02-16 04:42:28 +0100

    Task 0. Star Wars Characters--fix rewriting to add async

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
+const util = require('util');
+const requestPromise = util.promisify(request);
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (err, res, body) {
-  if (err) console.log(err);
-  const chars = JSON.parse(body).characters;
-  for (let i = 0; i < chars.length; ++i) {
-    setTimeout(() => {
-      request(chars[i], function (err, res, body) {
-        if (err) console.log(err);
-        console.log(JSON.parse(body).name);
-      }, 2000);
-    });
+
+async function getCharactersInRightOrder(url) {
+  try {
+    const response = await requestPromise(url);
+    const chars = JSON.parse(response.body).characters;
+    for (let i = 0; i < chars.length; ++i) {
+      const charResponse = await requestPromise(chars[i]);
+      console.log(JSON.parse(charResponse.body).name);
+    }
+  } catch (err) {
+    console.log(err);
   }
-});
+}
+
+getCharactersInRightOrder(url);
