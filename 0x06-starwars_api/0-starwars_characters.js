7801bdfb16f0344b1a35937a6a475e8f1189f2bd
moulineE <elmahdi.mouline@live.fr>
2024-02-16 01:00:41 +0100

    Tash 0. Star Wars Characters -fix1

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,10 +4,12 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request(url, function (err, res, body) {
   if (err) console.log(err);
   const chars = JSON.parse(body).characters;
-  chars.forEach((element) => {
-    request(element, function (err, res, body) {
-      if (err) console.log(err);
-      console.log(JSON.parse(body).name);
+  for (let i = 0; i < chars.length; ++i) {
+    setTimeout(() => {
+      request(chars[i], function (err, res, body) {
+        if (err) console.log(err);
+        console.log(JSON.parse(body).name);
+      }, 2000);
     });
-  });
+  }
 });
